1eac2b2f7000832e7e7b9ae4fa8424d7ef4f08d4
Lumiere Xyloto <740207968@qq.com>
2018-11-12 11:06:13 +0800

    finish typeList

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -2,7 +2,7 @@
   <div class="icons">
     <swiper :options="swiperOption">
       <swiper-slide v-for="(page,index) of pages" :key="index">
-        <div class="icon" v-for="item of page" :key="item.id">
+        <div class="icon" v-for="item of page" :key="item.id" @click="title=item.desc;typeStore=item.id;sendRequest()">
           <div class="icon-img">
             <img class="icon-img-content" :src="item.imgUrl">
           </div>
@@ -10,10 +10,37 @@
         </div>
       </swiper-slide>
     </swiper>
+
+    <!-- 根据兼职类型返回的列表 -->
+    <div class="recommend-title">{{title}}</div>
+    <ul>
+      <router-link
+        tag="li"
+        class="item border-bottom"
+        v-for="item of list"
+        :key="item.jobId"
+        :to="'/detail/' + item.jobId"
+      >
+        <div class="item-img-wrapper">
+          <img src="@/assets/images/logo.png" class="item-img">
+        </div>
+        <div class="item-info">
+          <p class="item-brief">{{item.brief}}</p>
+          <p class="item-time">{{item.location}}</p>
+          <p class="item-time">{{item.time}}</p>
+          <div class="reward-and-num">
+            <p class="item-reward">{{item.reward}}/{{item.rewardType}}</p>
+            <p class="item-num">供需:{{item.nowNum}}/{{item.hireNum}}</p>
+          </div>
+        </div>
+      </router-link>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import qs from 'qs'
 export default {
   name: 'HomeIcons',
   data () {
@@ -61,7 +88,10 @@ export default {
         id: '0',
         imgUrl: './static/images/0.png',
         desc: '其它'
-      }]
+      }],
+      title: '',
+      list: [],
+      typeStore: ''
     }
   },
   computed: {
@@ -75,6 +105,20 @@ export default {
         pages[page].push(item)
       })
       return pages
+    }
+  },
+  methods: {
+    sendRequest () {
+      axios.post('http://yian.our16.top:8080/yian/parttimeHall/seekByParttimeType.do', qs.stringify({
+        parttimeType: this.typeStore,
+        pageNum: '0',
+        onePageNum: '0'
+      }))
+        .then(this.sendRequestSucc)
+    },
+    sendRequestSucc (res) {
+      console.log(res)
+      this.list = res.data.data
     }
   }
 }
@@ -119,4 +163,44 @@ export default {
         color: $darkTextColor
         text-align: center
         ellipsis()
+  .recommend-title
+    margin-top: .2rem
+    background: #eeeeee
+    line-height: .8rem
+    text-indent: .2rem
+  .item
+    display flex
+    .item-img-wrapper
+      display flex
+      justify-content center
+      align-items center
+      height 2.2rem
+      .item-img
+        width 1.2rem
+        height 1.2rem
+        padding .3rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-time
+        ellipsis()
+        color #ADADAD
+        padding .1rem 0
+      .item-brief
+        ellipsis()
+        font-size .36rem
+        padding .1rem 0
+      .reward-and-num
+        display flex
+        justify-content space-between
+        padding-right .8rem
+        .item-reward
+          color #FE7418
+          font-size .32rem
+          padding .1rem 0
+        .item-num
+          color #ADADAD
+          font-size .3rem
+          padding .1rem 0
 </style>
